fc54cf36341675c47306662acae6549deac8080e
saninog07 <santino.gennuso.m@gmail.com>
2026-05-31 15:54:07 -0300

    MianMenu class creation with text display. And change the default resolution.

--- core/src/main/java/com/TfPSR/CucoProject/Main.java
+++ core/src/main/java/com/TfPSR/CucoProject/Main.java
@@ -1,32 +1,29 @@
 package com.TfPSR.CucoProject;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
+public class Main extends Game { //We use Game, because it has better methods of pause, play, and resume
+    private Batch batch;
     private Texture image;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
-    }
-
-    @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        setScreen(new MainMenu(this));
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+    }
+
+    public Batch getBatch(){
+        return batch;
     }
 }
